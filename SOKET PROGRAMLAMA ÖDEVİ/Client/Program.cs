d623a20a77be4eb770c0e40efeb59f99258d4d25
emrullahsytrk <41507439+emrullahsytrk@users.noreply.github.com>
2020-05-15 00:29:47 +0300

    Add files via upload

---
+++ SOKET PROGRAMLAMA ÖDEVİ/Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            s.Connect(IPAddress.Parse("127.0.0.1"), 600);
+            byte[] buffer = Encoding.ASCII.GetBytes(Console.ReadLine());
+            s.Send(buffer);
+            s.Close();
+            Console.Read();
+        }
+    }
+}
---
+++ SOKET PROGRAMLAMA ÖDEVİ/soketprogramming/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace soketprogramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                s.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 600));
+                s.Listen(9);
+                Socket client = s.Accept();
+                NetworkStream ns = new NetworkStream(client);
+                StreamReader sr = new StreamReader(ns);
+                Console.WriteLine(sr.ReadToEnd());
+                sr.Close();
+                ns.Close();
+                s.Shutdown(SocketShutdown.Receive);
+                client.Shutdown(SocketShutdown.Receive);
+            }
+            catch (SocketException exc)
+            {
+                Console.WriteLine("HATA OLUŞTU");
+            }
+            Console.Read();
+        }
+    }
+}
